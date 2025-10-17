9e43abeafd2594511cf9bfc14a8eb85ca7e6b472
Enzo Pinotti <enzopinottii@gmail.com>
2025-10-17 16:21:52 -0300

    add: cambios para meta

--- src/routes/webhookRouter.js
+++ src/routes/webhookRouter.js
@@ -6,9 +6,17 @@ import {
 
 const router = Router();
 
+// Responder HEAD con 200 (salud)
+router.head('/', (_req, res) => res.sendStatus(200));
+
+// Si viene GET sin hub.*, devolvemos 200 "ok" (ping)
+router.get('/', (req, res, next) => {
+  if (!req.query['hub.mode']) return res.status(200).type('text/plain').send('ok');
+  return next(); // sigue a la verificación real
+});
+
 // GET para verificación de Meta (hub.challenge)
 router.get('/', handleWhatsAppVerify);
-router.head('/', handleWhatsAppVerify);
 
 // POST para eventos/mensajes entrantes
 router.post('/', handleWhatsAppMessage);
